581fbf3712b7bb7de57714b0b28b6cb9c4f0d5f0
Jem Young <jemuel.young@gmail.com>
2015-01-12 01:14:24 -0500

    Update

--- learnclojure.clj
+++ learnclojure.clj
@@ -90,3 +90,22 @@
 
 
 ; (= (__ '(1 10 3)) 14)
+
+(fn [l] (reduce (fn [x, y] (+ x y)) 0 l))
+
+
+
+; Write a function which returns only the odd numbers from a sequence.
+
+; (= (__ #{1 2 3 4 5}) '(1 3 5))
+
+
+; (= (__ [4 2 1 6]) '(1))
+
+
+; (= (__ [2 2 4 6]) '())
+
+
+; (= (__ [1 1 1 3]) '(1 1 1 3))
+
+
