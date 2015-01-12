2abd493013060d80e8e1d6788334731bc6cb4c96
Jem Young <jemuel.young@gmail.com>
2015-01-11 23:53:54 -0500

    Update

--- learnclojure.clj
+++ learnclojure.clj
@@ -1,5 +1,4 @@
 ; Write a function which returns the second to last element from a sequence.
-; test not run  
 
 ; (= (__ (list 1 2 3 4 5)) 4)
 
@@ -11,7 +10,6 @@
 
 
 ; Write a function which returns the last element in a sequence.
-; test not run  
 
 ; (= (__ [1 2 3 4 5]) 5)
 
@@ -20,3 +18,24 @@
 ; (= (__ ["b" "c" "d"]) "d")
 
 #(first (reverse %))
+
+
+
+
+
+; Write a function which returns the Nth element from a sequence.
+; Restrictions: can't use nth
+
+; (= (__ '(4 5 6 7) 2) 6)
+
+
+; (= (__ [:a :b :c] 0) :a)
+
+
+; (= (__ [1 2 3 4] 1) 2)
+
+
+; (= (__ '([1 2] [3 4] [5 6]) 2) [5 6])
+
+(fn [l, n]  
+  (last (take (+ n 1) l)))
